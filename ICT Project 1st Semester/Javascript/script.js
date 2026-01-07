7228d8ec83d1ecfd50fe6c9c8eb28f6c7f60c042
Syed Sharjeel Ahmed <syedsharjeelahmed214@gmail.com>
2026-01-07 16:50:46 +0500

    Add files via upload

---
+++ ICT Project 1st Semester/Javascript/script.js
@@ -0,0 +1,13 @@
+  function changeImage(src) {
+    document.getElementById("mainImage").src = src;
+  }
+
+
+  function changeImage2(src) {
+    document.getElementById("mainImage2").src = src;
+  }
+
+    function changeImage3(src) {
+    document.getElementById("mainImage3").src = src;
+  }
+  
